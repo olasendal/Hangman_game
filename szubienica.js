bb555f8b78f531f29d57ff7a822eff6e84123532
Ola Sendal <43734526+olasendal@users.noreply.github.com>
2019-04-06 21:27:45 +1300

    Add files via upload

---
+++ szubienica.js
@@ -0,0 +1,116 @@
+var haslo = "I am gonna be programmer";
+haslo = haslo.toUpperCase();
+
+var dlugosc = haslo.length;
+var ile_wtop = 0;
+var haslo1 = "";
+
+for (i=0; i<dlugosc; i++)
+{
+  if (haslo.charAt(i) == " ") haslo1 = haslo1 + " ";
+  else haslo1 = haslo1 + "-";
+}
+
+function wypisz_haslo()
+{
+  document.getElementById("plansza").innerHTML = haslo1;
+}
+
+window.onload = start;
+
+var litery = new Array(25);
+
+litery[0] = "A";
+litery[1] = "B";
+litery[2] = "C";
+litery[3] = "D";
+litery[4] = "E";
+litery[5] = "F";
+litery[6] = "G";
+litery[7] = "H";
+litery[8] = "I";
+litery[9] = "J";
+litery[10] = "K";
+litery[11] = "L";
+litery[12] = "M";
+litery[13] = "N";
+litery[14] = "O";
+litery[15] = "P";
+litery[16] = "Q";
+litery[17] = "R";
+litery[18] = "S";
+litery[19] = "T";
+litery[20] = "U";
+litery[21] = "V";
+litery[22] = "W";
+litery[23] = "X";
+litery[24] = "Y";
+litery[25] = "Z";
+
+
+function start()
+{
+  var tresc_diva = "";
+
+  for (i=0; i <= 25; i++)
+  {
+    var element = "lit" + i;
+    tresc_diva = tresc_diva + '<div class="litera" onclick="sprawdz('+i+')"id="'+element+'">'+litery[i]+'</div>';
+  }
+
+  document.getElementById("alfabet").innerHTML = tresc_diva;
+
+
+  wypisz_haslo();
+}
+
+String.prototype.ustawZnak = function(miejsce, znak)
+{
+  if (miejsce>this.length-1) return this.toString();
+  else return this.substr(0, miejsce) + znak + this.substr(miejsce+1);
+}
+
+function sprawdz(nr)
+{
+
+  var trafiona = false;
+
+  for(i=0; i<dlugosc; i++)
+  {
+    if(haslo.charAt(i) == litery[nr])
+    {
+      haslo1 = haslo1.ustawZnak(i,litery[nr]);
+      trafiona = true;
+    }
+  }
+
+  if(trafiona == true)
+  {
+    var element = "lit" + nr;
+    document.getElementById(element).style.background = "#003300";
+    document.getElementById(element).style.color = "#00C000";
+    document.getElementById(element).style.cursor = "default";
+    wypisz_haslo();
+  }
+  else
+  {
+    var element = "lit" + nr;
+    document.getElementById(element).style.background = "#330000";
+    document.getElementById(element).style.color = "#C00000";
+    document.getElementById(element).style.cursor = "default";
+    document.getElementById(element).setAttribute("onclick",";");
+
+    //wtopy
+    ile_wtop++;
+    var obraz = "img/s" + ile_wtop + ".jpg";
+    document.getElementById("szubienica").innerHTML = '<img src="'+obraz+'" alt=""/>';
+  }
+
+  //wygrana
+  if(haslo == haslo1)
+  document.getElementById("alfabet").innerHTML = "Congrats! The secret phrase is: " + haslo + '<br/><br/><span class="reset" onclick="location.reload()">Play again<span/>';
+
+  //przegrana
+  if(ile_wtop>=9)
+  document.getElementById("alfabet").innerHTML = "Game over!" + '<br/><br/><span class="reset" onclick="location.reload()">Play again<span/>';
+}
